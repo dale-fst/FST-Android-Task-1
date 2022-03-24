d3aefd2345aa10201503de5c7da8651e6a89b309
Dale Moncayo <moncayo.dale@fullspeedtechnologies.com>
2022-03-24 10:09:31 +0800

    Implementing buildTypes resValues for playgoose url

--- app/src/main/java/com/fstdale/androidtask1/ui/pages/home/HomeViewModel.kt
+++ app/src/main/java/com/fstdale/androidtask1/ui/pages/home/HomeViewModel.kt
@@ -4,14 +4,16 @@ import android.app.Application
 import android.content.Intent
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.fstdale.androidtask1.R
 import com.fstdale.androidtask1.ui.pages.auth.SignupActivity
+
 
 class HomeViewModel(val app: Application) : AndroidViewModel(app) {
 
     var url = MutableLiveData<String>()
 
     fun setURL() {
-        url.postValue("https://staging-home-playgoose-standalone.dsign.gift/artist/playgoose")
+        url.postValue(app.getString(R.string.URL_PLAYGOOSE))
     }
 
     fun singUp() {
